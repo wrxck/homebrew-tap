1fa200a3fe731fd5c09ae2bf1d74df176dc2f16a
Matt Hesketh <matt@heskethwebdesign.co.uk>
2026-03-22 20:43:12 +0000

    feat: add agenttop formula v0.11.0

---
+++ Formula/agenttop.rb
@@ -0,0 +1,17 @@
+class Agenttop < Formula
+  desc "Real-time terminal dashboard for monitoring AI coding agent sessions"
+  homepage "https://github.com/wrxck/agenttop"
+  url "https://registry.npmjs.org/agenttop/-/agenttop-0.11.0.tgz"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/agenttop --version")
+  end
+end
